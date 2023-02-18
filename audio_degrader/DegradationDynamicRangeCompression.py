fc5d88e8a5a46a72be64f94b77b9f23f2061d3b8
Helene-Maxcici <119662709+Helene-Maxcici@users.noreply.github.com>
2023-02-18 21:10:21 +0100

    Adapt to in-memory transformation

--- audio_degrader/DegradationDynamicRangeCompression.py
+++ audio_degrader/DegradationDynamicRangeCompression.py
@@ -14,28 +14,35 @@ class DegradationDynamicRangeCompression(Degradation):
          "0",
          "Degree of compression. Presets from 0 (soft) to 3 (hard)")]
 
-    def apply(self, audio_file):
-        extra_tmp_path = audio_file.tmp_path + '.extra.wav'
+    def apply(self, audio):
+        
         degree = int(self.parameters_values['degree'])
         if degree == 1:
-            cmd = ("sox {0} {1} compand " +
-                   "0.01,0.20 -40,-10,-30 5")
+          attack_time = 0.01
+          decay_time = 0.20
+          tf_points = [(-100,-100),(-40,-40), (-10,-30)]
+          gain = 5
+
         elif degree == 2:
-            cmd = ("sox {0} {1} compand " +
-                   "0.01,0.20 -50,-50,-40,-30,-40,-10,-30 12")
+          attack_time = 0.01
+          decay_time = 0.20
+          tf_points = [(-100,-100),(-50.01,-50.01),(-50,-40), 
+                       (-30,-40),(-10,-30)]
+          gain = 12
         elif degree == 3:
-            cmd = ("sox {0} {1} compand " +
-                   "0.01,0.1 -70,-60,-70,-30,-70,0,-70 45")
-        cmd = cmd.format(audio_file.tmp_path,
-                         extra_tmp_path)
-        logging.info(cmd)
-        out, err, returncode = run(cmd)
-        if returncode != 0:
-            logging.debug(out)
-            logging.debug(err)
-            logging.error("Error running sox!")
-        y, sr = sf.read(extra_tmp_path)
+          attack_time = 0.01
+          decay_time = 0.10
+          tf_points = [(-100,-100),(-70,-70),(-30,-70),(0,-70)]
+          gain = 45
+    
+        tfm = sox.Transformer()
+        tfm.compand(attack_time = attack_time, decay_time = decay_time, 
+                    tf_points = tf_points)
+        tfm.gain(gain, normalize = False, limiter = False)
+        tfm.set_output_format(bits=32, channels=2)
+        y = tfm.build_array(input_array = audio.samples.T, 
+                            sample_rate_in = audio.sample_rate)
         y = y.T
-        os.remove(extra_tmp_path)
-        assert audio_file.sample_rate == sr
-        audio_file.samples = y
+
+        assert audio.samples.shape == y.shape
+        audio.samples = y
