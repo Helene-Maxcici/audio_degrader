efe001d1255e6af0a096d86d841082d8801b6db8
Helene-Maxcici <119662709+Helene-Maxcici@users.noreply.github.com>
2023-02-18 13:58:58 +0100

    Adapt to in-memory transformation

--- audio_degrader/DegradationTrim.py
+++ audio_degrader/DegradationTrim.py
@@ -7,8 +7,8 @@ class DegradationTrim(Degradation):
     description = "Trim audio from a given start time"
     parameters_info = [("start_time", 0.1, "Trim start [seconds]")]
 
-    def apply(self, audio_file):
+    def apply(self, audio):
         start_time = float(self.parameters_values["start_time"])
-        start_sample = int(start_time * audio_file.sample_rate)
-        audio_file.samples = audio_file.samples[
+        start_sample = int(start_time * audio.sample_rate)
+        audio.samples = audio.samples[
             :, start_sample:]
