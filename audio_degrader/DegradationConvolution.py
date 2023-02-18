a8606d8f0102e28071c1ab27ca94ccac3f5d8b27
Helene-Maxcici <119662709+Helene-Maxcici@users.noreply.github.com>
2023-02-18 13:22:26 +0100

    Correct upload

--- audio_degrader/DegradationConvolution.py
+++ audio_degrader/DegradationConvolution.py
@@ -5,7 +5,6 @@ import numpy as np
 from scipy import signal
 import os
 from .BaseDegradation import Degradation
-
 class DegradationConvolution(Degradation):
 
     name = "convolution"
@@ -50,24 +49,3 @@ class DegradationConvolution(Degradation):
                 x[channel, :], ir_x[channel, :], mode='full')[0:x.shape[1]]
         y = y_wet * level + x * (1 - level)
         audio.samples = y
-
-    def apply(self, audio_file):
-        ir_path = self.get_actual_impulse_response_path()
-        level = float(self.parameters_values['level'])
-        logging.info('Convolving with %s and level %f' % (ir_path, level))
-        x = audio_file.samples
-        extra_tmp_path = audio_file.tmp_path + '.extra.wav'
-        tfm = sox.Transformer()
-        tfm.rate(audio_file.sample_rate)
-        tfm.convert(n_channels=2, bitdepth=32)
-        tfm.build(ir_path, extra_tmp_path)
-
-        ir_x, _ = sf.read(extra_tmp_path)
-        ir_x = ir_x.T
-        os.remove(extra_tmp_path)
-        y_wet = np.zeros(x.shape)
-        for channel in [0, 1]:
-            y_wet[channel, :] = signal.fftconvolve(
-                x[channel, :], ir_x[channel, :], mode='full')[0:x.shape[1]]
-        y = y_wet * level + x * (1 - level)
-        audio_file.samples = y
