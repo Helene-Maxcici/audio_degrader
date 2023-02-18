18c15315e8c9338162594c939020852c6255b7b7
Helene-Maxcici <119662709+Helene-Maxcici@users.noreply.github.com>
2023-02-18 13:42:31 +0100

    Adapt to in-memory transformation

--- audio_degrader/DegradationGain.py
+++ audio_degrader/DegradationGain.py
@@ -9,10 +9,10 @@ class DegradationGain(Degradation):
     description = "Apply gain expressed in dBs"
     parameters_info = [("value", "6", "Gain value [dB]")]
 
-    def apply(self, audio_file):
+    def apply(self, audio):
         value = float(self.parameters_values["value"])
         logging.debug("Apply gain %f dB" % value)
-        x = audio_file.samples
+        x = audio.samples
         x = x * (10 ** (value / 20.0))  # linear value
         x = np.minimum(np.maximum(-1.0, x), 1.0)
-        audio_file.samples = x
+        audio.samples = x
