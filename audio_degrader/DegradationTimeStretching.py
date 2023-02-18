ed790c7578eb6206acdee5f3f38b0d86194894a5
Helene-Maxcici <119662709+Helene-Maxcici@users.noreply.github.com>
2023-02-18 13:58:10 +0100

    Adapt to in-memory transformation

--- audio_degrader/DegradationTimeStretching.py
+++ audio_degrader/DegradationTimeStretching.py
@@ -14,17 +14,16 @@ class DegradationTimeStretching(Degradation):
          "0.9",
          "Time stretch factor")]
 
-    def apply(self, audio_file):
+    def apply(self, audio):
         time_stretch_factor = float(
             self.parameters_values["time_stretch_factor"])
         logging.info(('Time stretching with factor %f' %
                       (time_stretch_factor)))
-        extra_tmp_path = audio_file.tmp_path + '.extra.wav'
+
         tfm = sox.Transformer()
         tfm.tempo(time_stretch_factor)
-        tfm.convert(n_channels=2, bitdepth=32)
-        tfm.build(audio_file.tmp_path, extra_tmp_path)
-        y, sr = sf.read(extra_tmp_path)
+        tfm.set_output_format(bits=32, channels=2)
+        y = tfm.build_array(input_array = audio.samples.T, 
+                            sample_rate_in = audio.sample_rate)
         y = y.T
-        os.remove(extra_tmp_path)
-        audio_file.samples = y
+        audio.samples = y
